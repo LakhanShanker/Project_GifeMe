7e26572a0221b7b04ef37b4d96239be1e6a32298
Lakhan <Lakhan.Shanker@wu.com>
2022-11-11 14:26:41 +0530

    hello first push

---
+++ App.js
@@ -0,0 +1,13 @@
+import { Provider } from 'react-redux';
+import './App.css';
+import Routers from './Routers';
+import store from './redux/store';
+function App() {
+  return (
+      <Provider store={store}>
+        <Routers/>
+      </Provider>    
+  );
+}
+
+export default App;
